74a90cb167732e41a8ae62976ffa9ab278665e13
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2025-10-22 20:05:15 +0200

    All aspects are improved. Impressum added

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 function init() {
+  loadBooksFromLocalStorage();
   renderBooks();
 }
-  function getCardHTML(book, i, commentsHtml){
-      return `
-        <h2>${book.name}</h2>
-        <img class="bookPNG" src="./assets/Book_png.png" alt="book" />
-        <div class="priceLike">
-          <p><strong>Preis:</strong> ${book.price.toFixed(2)} €</p>
-          <p>
-            <span class="like_BTN ${book.liked ? 'liked' : ''}" onclick="toggleLike(${i})" id="like-${i}">
-              ❤️ ${book.likes}
-            </span>
-          </p>
-        </div>
-        <p><strong>Autor:</strong> ${book.author}</p>
-        <p><strong>Erscheinungsjahr:</strong> ${book.publishedYear}</p>
-        <p><strong>Genre:</strong> ${book.genre}</p>
-        <div class="comments">
-          <h4>Kommentare:</h4>
-          ${commentsHtml}
-        </div>
-        <div class="comment-section">
-          <input class="comments_inputs" type="text" placeholder="Dein Name" id="name-${i}">
-          <input class="comments_inputs" type="text" placeholder="Schreibe dein Kommentar..." id="comment-${i}">
-          <button class="send_BTN" onclick="addComment(${i})">Senden</button>
-        </div>
-        `
-    }
+
+function getCardHTML(book, i, commentsHtml) {
+  return `
+    <h2>${book.name}</h2>
+    <img class="bookPNG" src="./assets/Book_png.png" alt="book" />
+    <div class="priceLike">
+      <p class="price_value"><strong class="price">Preis:</strong> ${book.price.toFixed(2)} €</p>
+      <p>
+        <span class="like_BTN ${book.liked ? 'liked' : ''}" onclick="toggleLike(${i})" id="like-${i}">
+        ${book.liked ? '❤️' : '♡'} ${book.likes}
+        </span>
+      </p>
+    </div>
+    <div class="author_sec">
+    <p><strong>Autor:</strong> ${book.author}</p>
+    <p><strong>Erscheinungsjahr:</strong> ${book.publishedYear}</p>
+    <p><strong>Genre:</strong> ${book.genre}</p>
+    </div>
+    <div class="comments">
+      <h4>Kommentare:</h4>
+      ${commentsHtml}
+    </div>
+    <div class="comment-section">
+      <input class="comments_inputs" type="text" placeholder="Dein Name" id="name-${i}">
+      <input class="comments_inputs" type="text" placeholder="Schreibe dein Kommentar..." id="comment-${i}">
+      <button class="send_BTN" onclick="addComment(${i})">Senden</button>
+    </div>
+  `;
+}
 
 function renderBooks() {
   var container = document.getElementById('bookContainer');
@@ -41,15 +45,13 @@ function renderBooks() {
     var commentsHtml = '';
     for (var j = 0; j < book.comments.length; j++) {
       var comment = book.comments[j];
-      commentsHtml += '<p><strong class="commenter">' + comment.name + '</strong>: ' + comment.comment + '</p>';
+      commentsHtml += '<p><strong class="commenter">' + comment.name + '</strong> <br> ' + comment.comment + ' <br><br> </p>';
     }
 
     card.innerHTML = getCardHTML(book, i, commentsHtml);
     container.appendChild(card);
   }
 }
-
-
 
 function toggleLike(index) {
   var book = books[index];
@@ -62,6 +64,7 @@ function toggleLike(index) {
     book.likes++;
   }
 
+  saveBooksToLocalStorage();
   renderBooks();
 }
 
@@ -78,6 +81,18 @@ function addComment(index) {
     nameInput.value = '';
     commentInput.value = '';
 
+    saveBooksToLocalStorage();
     renderBooks();
+  }
+}
+
+function saveBooksToLocalStorage() {
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function loadBooksFromLocalStorage() {
+  var storedBooks = localStorage.getItem('books');
+  if (storedBooks) {
+    books = JSON.parse(storedBooks);
   }
 }
